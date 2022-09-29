3e001a0486b2f8fe44fdf3f16906c70bfa44aa29
Ouail-01 <ouaillaarabi@gmail.com>
2022-09-30 00:43:01 +0100

    complete Nav

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const navlinks = document.querySelector('.nav-links');
+
+
+menuBtn.addEventListener('click',() => {
+    navlinks.classList.toggle('mobile-menu');
+})
